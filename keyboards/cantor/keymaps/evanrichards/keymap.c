9152185ba1d26cb7d492038d7b9c7f1ffa3a8b4e
Evan Richards <tevanrichards@gmail.com>
2022-09-15 22:52:19 -0700

    added symetric symbols layer

--- keyboards/cantor/keymaps/evanrichards/keymap.c
+++ keyboards/cantor/keymaps/evanrichards/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,        KC_Q,       KC_W,         KC_F,        KC_P,        KC_B,            KC_J,        KC_L,        KC_U,        KC_Y,     KC_SCLN, KC_NO,
         KC_NO, CTL_T(KC_A), SFT_T(KC_R), OPT_T(KC_S), CMD_T(KC_T), ALL_T(KC_G),     ALL_T(KC_M), CMD_T(KC_N), OPT_T(KC_E), SFT_T(KC_I), LT(2, KC_O), KC_NO,
         KC_NO,        KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,             KC_K,       KC_H,     KC_COMM,      KC_DOT,     KC_SLSH, KC_NO,
-                                  KC_LEFT_CTRL, LT(1,KC_SPC), MT(MOD_LSFT, KC_TAB),   KC_BACKSPACE, LT(3,KC_ENTER), KC_ESCAPE
+                                  KC_LEFT_CTRL, LT(1,KC_SPC), MT(MOD_LSFT, KC_TAB),   LT(4,KC_BACKSPACE), LT(3,KC_ENTER), KC_ESCAPE
     ),
     [1] = LAYOUT_split_3x6_3(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         KC_GRAVE,  KC_7,  KC_8,  KC_9, KC_BSLS, KC_NO,
@@ -43,6 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, S(KC_QUOT), S(KC_6),  S(KC_5),  S(KC_4),  S(KC_MINS),                   KC_NO, KC_LCTL, KC_LSFT, KC_LALT, KC_LCMD, KC_NO,
         KC_NO,      KC_NO, S(KC_3),  S(KC_2),  S(KC_1),      KC_EQL,                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
                                        KC_NO,   KC_NO,  KC_NO,    KC_NO, KC_NO,     KC_NO
+    ),
+
+    [4] = LAYOUT_split_3x6_3(
+        KC_NO, KC_NO,      KC_NO, S(KC_LBRC), S(KC_RBRC), KC_NO,                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+        KC_NO, KC_NO,      KC_NO,    S(KC_9),    S(KC_0), KC_NO,                   KC_NO, KC_LCTL, KC_LSFT, KC_LALT, KC_LCMD, KC_NO,
+        KC_NO, KC_NO, S(KC_COMM),    KC_LBRC,    KC_RBRC, KC_NO,                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+                                                 KC_NO,   KC_NO,  KC_NO,    KC_NO, KC_NO,     KC_NO
     )
 };
 
