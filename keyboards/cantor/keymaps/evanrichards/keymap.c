7bcdfbfbd24433a6e5fd57a62f41eb98554e3986
Evan Richards <tevanrichards@gmail.com>
2022-11-04 10:26:40 -0700

    fix typo in layer 0

--- keyboards/cantor/keymaps/evanrichards/keymap.c
+++ keyboards/cantor/keymaps/evanrichards/keymap.c
@@ -1,6 +1,6 @@
 
 #include QMK_KEYBOARD_H
-
+// To compile: qmk compile -kb cantor -km evanrichards
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * Layer 0 -  Letters
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [0] = LAYOUT_split_3x6_3(
         KC_NO,        KC_Q,       KC_W,         KC_F,        KC_P,        KC_B,            KC_J,        KC_L,        KC_U,        KC_Y,     KC_SCLN, KC_NO,
-        KC_NO, CTL_T(KC_A), OPT_T(KC_R), CMT_T(KC_S), LSFT_T(KC_T), ALL_T(KC_G),     ALL_T(KC_M), RSFT_T(KC_N), CMD_T(KC_E),OPT_T(KC_I), CTL_T(KC_O), KC_NO,
+        KC_NO, CTL_T(KC_A), OPT_T(KC_R), CMD_T(KC_S), LSFT_T(KC_T), ALL_T(KC_G),     ALL_T(KC_M), RSFT_T(KC_N), CMD_T(KC_E),OPT_T(KC_I), CTL_T(KC_O), KC_NO,
         KC_NO,        KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,             KC_K,       KC_H,     KC_COMM,      KC_DOT,     KC_SLSH, KC_NO,
                                          KC_LEFT_CTRL, LT(1,KC_SPC), LT(2, KC_TAB),   LT(4,KC_BACKSPACE), LT(3,KC_ENTER), KC_ESCAPE
     ),
